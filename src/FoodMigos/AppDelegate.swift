2945aa2a563dcfdbb498c4dad2a22e314caaab5f
assistirtvbr-rgb <assistirtvbr@gmail.com>
2026-08-29 19:18:00 -0300

    Fix startup crash: remove dummy Firebase initialization

--- src/FoodMigos/AppDelegate.swift
+++ src/FoodMigos/AppDelegate.swift
@@ -11,20 +11,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        FirebaseApp.configure()
-
-        Messaging.messaging().delegate = self
         UNUserNotificationCenter.current().delegate = self
 
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
         UNUserNotificationCenter.current().requestAuthorization(
             options: authOptions,
             completionHandler: { granted, error in
-                if granted {
-                    DispatchQueue.main.async {
-                        application.registerForRemoteNotifications()
-                    }
-                }
+                print("Notification permission granted: \(granted)")
             })
 
         return true
