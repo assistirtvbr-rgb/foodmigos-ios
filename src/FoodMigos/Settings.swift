c72e0501886c14c5e305b3f1cbac37bb646dea3b
assistirtvbr-rgb <assistirtvbr@gmail.com>
2026-08-31 19:43:06 -0300

    Update Settings.swift

--- src/FoodMigos/Settings.swift
+++ src/FoodMigos/Settings.swift
@@ -8,11 +8,10 @@ struct Cookie {
 let gcmMessageIDKey = "00000000000" // update this with actual ID if using Firebase 
 
 // URL for first launch
-let rootUrl = URL(string: "https://rogerioberto.com/foodmigos/index.php")!
+let rootUrl = URL(string: "https://foodmigos.com/index.php")!
 
-// allowed origin is for what we are sticking to pwa domain
-// This should also appear in Info.plist
-let allowedOrigins: [String] = ["rogerioberto.com"]
+let allowedOrigins: [String] = ["foodmigos.com"]
+
 
 // auth origins will open in modal and show toolbar for back into the main origin.
 // These should also appear in Info.plist
